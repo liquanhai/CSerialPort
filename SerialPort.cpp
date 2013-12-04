0bbdb8f3cc2daaa8b5fcd52055d7432053624894
LI Quanhai <quanhailee@126.com>
2013-12-04 16:55:39 +0800

    next hand over

--- SerialPort.cpp
+++ SerialPort.cpp
@@ -11,7 +11,7 @@
 **
 **	AUTHOR				Remon Spekreijse
 **
-**  2007-12-25 mrlong https://code.google.com/p/mycom/
+**  2007-12-25 mrlong    https://code.google.com/p/mycom/
 **  2011-11-06 liquanhai http://blog.csdn.net/liquanhai/article/details/6941574
 **  2013-12-04 viruscamp
 **
